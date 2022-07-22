2e86c1e6329d00ede761231ba8f6501a8acf367b
Us3rL0sT <akshencev04@mail.ru>
2022-07-22 22:13:06 +0500

    add-five-add-restartfive

--- src/main/java/com/first_app/cattimer/MainActivity.java
+++ src/main/java/com/first_app/cattimer/MainActivity.java
@@ -20,23 +20,29 @@ import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private float CurrentProgress = 100;
+    private float CurrentProgress = 30; // начинать с (-1)
     private ProgressBar progressBar;
 
-    private static final long START_TIME_IN_MILLIS = 100000; // 1500000
+    private static final long START_TIME_IN_MILLIS = 10 * 1000; // 1500 сек
+    private static final long REST_TIME_IN_MILLIS = 8 * 1000; // 300 сек
 
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
+    private Button mButtonStartPauseRest;
     private Button mButtonReset;
+    private Button mRestButtonReset;
 
     private CountDownTimer mCountDownTimer;
 
     private boolean mTimerRunning;
 
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private long mRestLeftInMillis = REST_TIME_IN_MILLIS;
 
     private GifImageView cat_move;
     private GifImageView cat_fall;
+
+    private TextView current_action;
 
 
 
@@ -52,10 +58,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        current_action = findViewById(R.id.current_action);
+
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
 
         mButtonStartPause = findViewById(R.id.button_start_pause); // кнопка начала отсчета
+        mButtonStartPauseRest = findViewById(R.id.button_start_pause_rest); // кнопка начала отсчета отдыха
         mButtonReset = findViewById(R.id.button_restart);
+        mRestButtonReset = findViewById(R.id.button_rest_restart);
 
         progressBar = findViewById(R.id.progressBar);
 
@@ -84,6 +94,29 @@ public class MainActivity extends AppCompatActivity {
                     ((GifDrawable)cat_move.getDrawable()).start();
                     CurrentProgress += 1;
                     startTimer();
+
+
+                }
+            }
+        });
+
+        mButtonStartPauseRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cat_move.setVisibility(View.VISIBLE);
+                cat_fall.setVisibility(View.INVISIBLE);
+
+                if (mTimerRunning) {
+                    ((GifDrawable)cat_move.getDrawable()).stop();
+                    pauseTimerRest();
+                } else {
+                    cat_move.setVisibility(View.VISIBLE);
+                    ((GifDrawable)cat_move.getDrawable()).start();
+                    CurrentProgress += 1;
+                    restTimer();
+
+
                 }
             }
         });
@@ -92,6 +125,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 resetTimer();
+            }
+        });
+
+        mRestButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetRestTimer();
             }
         });
 
@@ -110,30 +150,62 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                mTimerRunning = false;
-//                mButtonStartPause.setText("Start");
+
+                mButtonReset.setVisibility(View.INVISIBLE);
+                mButtonStartPauseRest.setVisibility(View.VISIBLE);
                 mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
+                restTimer();
             }
         }.start();
 
 
 
         mTimerRunning = true;
-//        mButtonStartPause.setText("pause");
         mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void restTimer() { // 5 минутный таймер
+        mCountDownTimer = new CountDownTimer(mRestLeftInMillis, 1000) {
+            @Override
+            public void onTick(long restMillisUntilFinished) {
+                mRestLeftInMillis = restMillisUntilFinished;
+                CurrentProgress = 90; // обновляется до 100, но время не обновляется
+                progressBar.setProgress((int)CurrentProgress); // установка значения
+                CurrentProgress -= 1; // значение прогресс бара
+                restUpdateCountDownText();
+                current_action.setText("Отдых");
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                ((GifDrawable)cat_move.getDrawable()).stop();
+            }
+        }.start();
+
+
+
+
+
+        mTimerRunning = true;
+        mRestButtonReset.setVisibility(View.INVISIBLE);
     }
 
     private void pauseTimer() {
         mCountDownTimer.cancel();
         mTimerRunning = false;
-//        mButtonStartPause.setText("Start");
         mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void pauseTimerRest() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mRestButtonReset.setVisibility(View.VISIBLE);
     }
 
     private void resetTimer() {
 
-        CurrentProgress = 100;
+        CurrentProgress = 60; // начинать с (-1)
         mTimeLeftInMillis = START_TIME_IN_MILLIS;
         updateCountDownText();
         cat_move.setVisibility(View.INVISIBLE);
@@ -141,6 +213,21 @@ public class MainActivity extends AppCompatActivity {
 
         mButtonReset.setVisibility(View.INVISIBLE);
         mButtonStartPause.setVisibility(View.VISIBLE);
+        ((GifDrawable)cat_fall.getDrawable()).reset();
+        gifTimer();
+
+    }
+
+    private void resetRestTimer() {
+
+        CurrentProgress = 30; // начинать с (-1)
+        mRestLeftInMillis = REST_TIME_IN_MILLIS;
+        restUpdateCountDownText();
+        cat_move.setVisibility(View.INVISIBLE);
+        cat_fall.setVisibility(View.VISIBLE);
+
+        mRestButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPauseRest.setVisibility(View.VISIBLE);
         ((GifDrawable)cat_fall.getDrawable()).reset();
         gifTimer();
 
@@ -168,6 +255,16 @@ public class MainActivity extends AppCompatActivity {
 
         mTextViewCountDown.setText(timeLeftFormatted);
     }
+
+    private void restUpdateCountDownText() {
+        int minutes = (int) (mRestLeftInMillis / 1000) / 60;
+        int seconds = (int) (mRestLeftInMillis / 1000) % 60;
+
+        String timeRestLeftFormatted = String.format(Locale.getDefault(), "%02d : %02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeRestLeftFormatted);
+    }
+
 
 
 
